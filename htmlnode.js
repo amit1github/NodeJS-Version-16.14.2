0b7ae1869b33a2035e2d5ebf3070ddac8f36b4cf
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2022-05-03 20:41:54 +0530

    html with nodejs using path and removing extensions

--- htmlnode.js
+++ htmlnode.js
@@ -6,6 +6,19 @@ const app = express()
 const publicPath = path.join(__dirname, 'public')
 console.log(publicPath); 
 
-app.use(express.static(publicPath))
+// app.use(express.static(publicPath))
+
+// put underscore to know that it is not in use.
+app.get('/', (_, res) => {
+    res.sendFile(`${publicPath}/index.html`)
+})
+
+app.get('/about', (_, res) => {
+    res.sendFile(`${publicPath}/about.html`)
+})
+
+app.get('*', (_, res) => {
+    res.sendFile(`${publicPath}/404page.html`)
+})
 
 app.listen(4000) 
