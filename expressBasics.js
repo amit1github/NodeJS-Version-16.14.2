504e60c91e0ea70388b2a505620a92c0a9cb6dae
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2022-05-03 19:35:30 +0530

    express basics and routing and req, res

--- expressBasics.js
+++ expressBasics.js
@@ -3,8 +3,12 @@ const res = require('express/lib/response')
 const app = express()
 
 app.get('/',(req, res) => {
+    console.log("data sent by browser", req.query.name);
     res.send("Home Page")
 })
+
+//from search http://localhost:5000/?name=%22amit%22
+//from terminal data sent by browser "amit"
 
 app.get('/about', (req, res) => {
     res.send("About Page")
