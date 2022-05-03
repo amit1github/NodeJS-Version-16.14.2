df5a0a43f6ef73c2f8f57fb6f174416ab0b8043c
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2022-05-03 21:02:30 +0530

    template engine ejs

--- htmlnode.js
+++ htmlnode.js
@@ -4,13 +4,24 @@ const path = require('path')
 const app = express()
 
 const publicPath = path.join(__dirname, 'public')
-console.log(publicPath); 
+// console.log(publicPath); 
 
 // app.use(express.static(publicPath))
+
+app.set('view engine', 'ejs')
 
 // put underscore to know that it is not in use.
 app.get('/', (_, res) => {
     res.sendFile(`${publicPath}/index.html`)
+})
+
+app.get('/profile', (_, res) => {
+    const user = {
+        name: "Amit",
+        email: "a@gmail.com",
+        city: "bihar"
+    }
+    res.render('profile', {user})
 })
 
 app.get('/about', (_, res) => {
