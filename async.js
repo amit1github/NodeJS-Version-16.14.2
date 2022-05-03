d744e4e349ab8348f800ca537a9cf550bac34002
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2022-05-03 12:31:01 +0530

    Asynchronous nodeJS

---
+++ async.js
@@ -0,0 +1,24 @@
+// console.log("start exe...");
+
+// setTimeout(() => {
+//   console.log("logic exe...");
+// }, 2000);
+
+// console.log("complete exe...");
+
+
+let a = 10
+let b = 0
+
+setTimeout(() => {
+    b=10
+}, 2000);
+
+console.log(a+b);
+
+/* 
+Synchronous: In synchornous operations tasks are perfromed one at a time.
+
+Asynchrounous: In asynchornous operations, second task do not wait to finish first task.
+
+*/
