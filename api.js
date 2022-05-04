b8fb226356d18edad1df21fae186dde9d4f993dd
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2022-05-04 23:11:38 +0530

    api post

--- api.js
+++ api.js
@@ -2,11 +2,19 @@ const express = require("express");
 const dbConnect = require("./mongoDB01");
 const app = express();
 
+app.use(express.json()) // to get data from postman
+
 app.get("/", async (req, res) => {
   let data = await dbConnect()
   data = await data.find().toArray()
   // console.log(data); 
   res.send(data);
 });
+
+app.post('/', async (req, res) => {
+  let data = await dbConnect()
+  let result = await data.insert(req.body)
+  res.send(result)
+})
 
 app.listen(3000);
