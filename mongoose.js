708eab1b1173e34993b142170b3806879b641c23
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2022-05-05 01:55:00 +0530

    crud with node and mongoose

--- mongoose.js
+++ mongoose.js
@@ -1,15 +1,47 @@
 const mongoose = require("mongoose");
 
-const main = async () => {
-  await mongoose.connect("mongodb://localhost:27017/e-comm");
-  const ProductSchema = new mongoose.Schema({
-    name: String,
-    price:Number
+mongoose.connect("mongodb://localhost:27017/e-comm");
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  brand: String,
+  category: String,
+});
+
+const saveInDB = async () => {
+  const Product = mongoose.model("products", productSchema);
+  let data = new Product({
+    name: "M16",
+    price: 100,
+    brand: "samsung",
+    category: "Mobile",
   });
-  const ProductsModel = mongoose.model("products", ProductSchema);
-  let data = new ProductsModel({ name: "M12", price: 100 });
   let result = await data.save();
   console.log(result);
 };
+// saveInDB()
 
-main();
+const updateInDB = async () => {
+  const Product = mongoose.model("products", productSchema);
+  let data = await Product.updateOne(
+    { name: "iphone" },
+    { $set: { brand: "apple", name: "iphone7"} }
+  );
+  console.log(data);
+};
+// updateInDB();
+
+const deleteInDB = async () => {
+  const Product = mongoose.model("products", productSchema);
+    let data = await Product.deleteOne({ name: "iphone7" })
+    console.log(data);
+}
+// deleteInDB()
+
+const findInDB = async () => {
+  const Product = mongoose.model("products", productSchema);
+    let data = await Product.find({name: "mi note"})
+    console.log(data);
+}
+// findInDB()
+
